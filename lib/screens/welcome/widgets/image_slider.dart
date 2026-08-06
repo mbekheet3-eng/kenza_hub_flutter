51420414def4bc374a58776ede5712815dbf5c3f
mbekheet3-eng <mbekheet3@gmail.com>
2026-08-06 16:19:14 +0300

    Update image_slider.dart

--- lib/screens/welcome/widgets/image_slider.dart
+++ lib/screens/welcome/widgets/image_slider.dart
@@ -1,1 +1,46 @@
+import 'package:flutter/material.dart';
 
+class ImageSlider extends StatelessWidget {
+  const ImageSlider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final images = [
+      "assets/images/welcome_1.png",
+      "assets/images/welcome_2.png",
+      "assets/images/welcome_3.png",
+    ];
+
+    return SizedBox(
+      height: 220,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: images.length,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        itemBuilder: (context, index) {
+          return Container(
+            width: 160,
+            margin: const EdgeInsets.only(right: 14),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.08),
+                  blurRadius: 12,
+                  offset: const Offset(0, 5),
+                ),
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                images[index],
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
