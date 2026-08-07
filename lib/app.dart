7bc2f3429444e81e2c3c0364e446352e83427f7d
mbekheet3-eng <mbekheet3@gmail.com>
2026-08-07 18:11:54 +0300

    Update lib/app.dart — KenzaHubApp single definition

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:kenza_hub_flutter/config/app_theme.dart';
 
+class KenzaHubApp extends StatelessWidget {
+  const KenzaHubApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      home: const Placeholder(),
+    );
+  }
+}
