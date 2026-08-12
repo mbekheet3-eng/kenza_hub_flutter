18dbe782793b3dbd0fd7e7b0e40e03f1f9442799
mbekheet3-eng <mbekheet3@gmail.com>
2026-08-12 18:36:51 +0300

    Update footer_links.dart

--- lib/screens/welcome/widgets/footer_links.dart
+++ lib/screens/welcome/widgets/footer_links.dart
@@ -14,49 +14,45 @@ class FooterLinks extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        bottom: 20,
-        left: 24,
-        right: 24,
-      ),
-      child: Wrap(
-        alignment: WrapAlignment.center,
-        spacing: 16,
-        runSpacing: 8,
-        children: [
-          TextButton(
-            onPressed: onAbout,
-            child: const Text(
-              'من نحن',
-              style: TextStyle(
-                color: Color(0xFF0F172A),
-                fontWeight: FontWeight.w500,
-              ),
+    return Wrap(
+      alignment: WrapAlignment.center,
+      spacing: 12,
+      runSpacing: 2,
+      children: [
+        TextButton(
+          onPressed: onAbout,
+          child: const Text(
+            'من نحن',
+            style: TextStyle(
+              color: Color(0xFF0F172A),
+              fontSize: 13,
+              fontWeight: FontWeight.w500,
             ),
           ),
-          TextButton(
-            onPressed: onTerms,
-            child: const Text(
-              'الشروط والأحكام',
-              style: TextStyle(
-                color: Color(0xFF0F172A),
-                fontWeight: FontWeight.w500,
-              ),
+        ),
+        TextButton(
+          onPressed: onTerms,
+          child: const Text(
+            'الشروط والأحكام',
+            style: TextStyle(
+              color: Color(0xFF0F172A),
+              fontSize: 13,
+              fontWeight: FontWeight.w500,
             ),
           ),
-          TextButton(
-            onPressed: onPrivacy,
-            child: const Text(
-              'سياسة الخصوصية',
-              style: TextStyle(
-                color: Color(0xFF0F172A),
-                fontWeight: FontWeight.w500,
-              ),
+        ),
+        TextButton(
+          onPressed: onPrivacy,
+          child: const Text(
+            'سياسة الخصوصية',
+            style: TextStyle(
+              color: Color(0xFF0F172A),
+              fontSize: 13,
+              fontWeight: FontWeight.w500,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
