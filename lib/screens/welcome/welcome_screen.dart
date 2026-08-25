4fa1ebb97d1d9f2d7115b783d0c76a1f57ba2791
mbekheet3-eng <mbekheet3@gmail.com>
2026-08-25 03:21:18 +0300

    Update welcome_screen.dart

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:kenza_hub_flutter/core/localization/app_localizations.dart';
-=======
 import '../../core/localization/app_localizations.dart';
 import 'welcome_actions.dart';
 import 'welcome_footer.dart';
->>>>>>> origin/test_build
 import 'welcome_image_rows.dart';
 import 'welcome_logo.dart';
 import 'language_selector.dart';
@@ -15,13 +11,8 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    final screenHeight = MediaQuery.sizeOf(context).height;
-=======
     final strings = AppLocalizations.of(context);
     final screenHeight = MediaQuery.sizeOf(context).height;
-
->>>>>>> origin/test_build
     final isSmallScreen = screenHeight < 700;
 
     return Scaffold(
@@ -43,12 +34,7 @@ class WelcomeScreen extends StatelessWidget {
                         vertical: isSmallScreen ? 8 : 12,
                       ),
                       child: Row(
-<<<<<<< HEAD
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-=======
-                        mainAxisAlignment:
-                            MainAxisAlignment.spaceBetween,
->>>>>>> origin/test_build
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: const [
                           WelcomeLogo(),
@@ -56,20 +42,12 @@ class WelcomeScreen extends StatelessWidget {
                         ],
                       ),
                     ),
-
                     SizedBox(
-<<<<<<< HEAD
-                      height: isSmallScreen ? 235 : 275,
-                      child: const Center(
-=======
                       height: isSmallScreen ? 225 : 275,
-                      child: const Align(
-                        alignment: Alignment.center,
->>>>>>> origin/test_build
+                      child: const Center(
                         child: WelcomeImageRows(),
                       ),
                     ),
-
                     Padding(
                       padding: EdgeInsets.fromLTRB(
                         24,
@@ -80,11 +58,7 @@ class WelcomeScreen extends StatelessWidget {
                       child: Column(
                         children: [
                           Text(
-<<<<<<< HEAD
-                            AppLocalizations.of(context).welcomeTitle,
-=======
                             strings.welcomeTitle,
->>>>>>> origin/test_build
                             textAlign: TextAlign.center,
                             style: TextStyle(
                               fontSize: isSmallScreen ? 24 : 26,
@@ -92,25 +66,16 @@ class WelcomeScreen extends StatelessWidget {
                               color: const Color(0xFF0F172A),
                               height: 1.35,
                             ),
-                          ),
-<<<<<<< HEAD
+                         ),
                           const SizedBox(height: 8),
-                          Text(
-                            AppLocalizations.of(context).welcomeSubtitle,
-=======
-
-                          const SizedBox(height: 8),
-
                           Text(
                             strings.welcomeSubtitle,
->>>>>>> origin/test_build
                             textAlign: TextAlign.center,
                             style: TextStyle(
                               fontSize: isSmallScreen ? 15 : 16,
                               color: const Color(0xFF64748B),
                             ),
                           ),
-<<<<<<< HEAD
                           SizedBox(
                             height: isSmallScreen ? 16 : 22,
                           ),
@@ -118,19 +83,6 @@ class WelcomeScreen extends StatelessWidget {
                           SizedBox(
                             height: isSmallScreen ? 10 : 16,
                           ),
-=======
-
-                          SizedBox(
-                            height: isSmallScreen ? 16 : 22,
-                          ),
-
-                          const WelcomeActions(),
-
-                          SizedBox(
-                            height: isSmallScreen ? 10 : 16,
-                          ),
-
->>>>>>> origin/test_build
                           const WelcomeFooter(),
                         ],
                       ),
